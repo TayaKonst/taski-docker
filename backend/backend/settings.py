d4677d315963a3cfa99b7d01058c03551065bba9
Taya Konstantinova <antinomiyaya@gmail.com>
2026-06-09 12:02:42 +0800

    fix: add CSRF_TRUSTED_ORIGINS for taskikaski.duckdns.org

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,3 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://taskikaski.duckdns.org'
+]
